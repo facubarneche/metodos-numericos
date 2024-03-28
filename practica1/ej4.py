42d1a236bcc2980ecc2a13986b15f1bb9fd5a4bb
Facundo Barneche <fh.barneche@gmail.com>
2024-03-28 17:48:32 -0300

    Se agrege ej 4

---
+++ practica1/ej4.py
@@ -0,0 +1,30 @@
+# 4 Indicar cómo se puede usar el método de Newton para calcular la función
+# f(x) = ln x para x > 0, usando otras operaciones: +. -, *, / y exponencial. Discutir una posible
+# implementación práctica de esto
+
+import math;
+
+# Funciones
+
+# f1(x) = √x
+def f(x): return math.log(x)
+
+# Derivadas
+def df(x): return 1 / (2 * math.sqrt(x))
+
+
+# Newton
+def newton(x0, x1):
+    x2 = x1 - ( (f(x1) * (x1 - x0)) / (f(x1) - f(x0)))
+    print(x2)
+    x0 = x1
+    x1 = x2
+
+newton(1, 10)
+
+#******************************************************************************
+#******************************** Observaciones *******************************
+#******************************************************************************
+# Este metodo se asemeja a Newton, vale destacar que se agrega una X arbitrariamente mas (X1 y X2)
+# Este caso en general le cuesta mucho aproximar mientras mas lejos esten las X1 y X2 de la raiz
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
