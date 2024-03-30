4db595a7635c14d1ecccb274961bb51da4a9e414
Facundo Barneche <fh.barneche@gmail.com>
2024-03-30 16:27:12 -0300

    se agrega el ej 12

--- practica1/ej12.py
+++ practica1/ej12.py
@@ -1,58 +1,16 @@
-# 12.
-# a) ¿Qué ocurriría si se usa el método de Newton para resolver una ecuación lineal con una
-# incógnita? Analizar todos los casos.
-# b) Misma pregunta, qué ocurriría, con el método de bisección.
-# c) ¿Y con el método secante?
+# 13 El método de bisección tiene entre otros requerimientos que al comienzo se cuente con
+# dos números reales a y b tales que f(a) y f(b) tengan distinto signo. Si bien eso puede ser
+# costoso, mostrar un algoritmo aproximado que converja hallando tales a y b en caso en que
+# existan. (Nota: si no hubiera tales valores, no se pide nada del algoritmo, que incluso podría
+# colgarse.)
 
-import math
+def esPosibleBiseccion(a, b, func):
+    if(func(a) * func(b) < 0): print(func(a), func(b))
+    else: print('No se puede hacer bisección con estos parametros', func(a), func(b))
 
-# Funciones
-def fa(x): return x * 2 + 1
-
-
-# Derivadas
-def dfa(x): return 2
-
-# Newton
-def newton(x0, f, df):
-    x1 = x0 - (f(x0) / df(x0))
-    print(x1)
-    x0 = x1
-
-# Bisección
-def biseccion(a, b, e, myFunc):
-    p = (a + b) / 2
-    print(p)
-    if((b - a) < e):
-        print(p, ' (Raiz Apróximada)')
-        return p
-    if(myFunc(p) > 0): return biseccion(a, p, e, myFunc)
-    else: return biseccion(p, b, e, myFunc)
-
-# Secante
-def secante (x0, x1):
-    x2 = x1 - (fa(x1) * (x1 - x0) / (fa(x1) - fa(x0)))
-    print(x2)
-
-
-print('Newton==========================')
-newton(-2000000, fa, dfa)
-print('Bisección=======================')
-biseccion(-2000000, 2000000, 0.001, fa)
-print('Secante=========================')
-secante(-2000000, 2000000)
-
-
+esPosibleBiseccion(-20, 2, lambda x: x ** 3 + x - 10)
 #******************************************************************************
 #******************************** Observaciones *******************************
 #******************************************************************************
-# Netwon
-# Se puede notar que con funciones lineales Newton encuentra la raiz en la primera iteración
-#
-# Biseccion
-# En el metodo de bisección si bien puede encontrarse una raiz en la primera iteración, mayormente
-# necesitará un numero mayor de iteraciones ya que depende muchisimo de los parametros de busqueda recibidos
-# 
-# Secante
-# Se puede notar que secante se asemeja a Newton en todo sentido ya que tambien es capas de encontrar
-# la raiz de una función lineal en una interación
+# Es una función para descubrir si es posible o no realizar el metodo de bisección con los parametros
+# recibidos
