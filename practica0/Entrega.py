1dead6a5cab35f7615d1d46e325b1aca5b0948c8
Facundo Barneche <fh.barneche@gmail.com>
2024-04-19 21:02:29 -0300

    [FEATURE] Se agrega grafico torta y documentacion del sistema

--- practica0/Entrega.py
+++ practica0/Entrega.py
@@ -2,15 +2,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Metodo de Trisección
-def trisection(f: Callable[[float], float], a: float, b: float) -> float:
-    count = 0
+# Se crean 2 variables globales para luego podes accederlas en el grafico de torta
+globalCountTrisec = 0
+globalCountBisec = 0
+
+#******************************************************************************
+#********************************** Trisección ********************************
+#******************************************************************************
+
+def trisection(f, a: float, b: float) -> float:
+    """
+    Esta función implementa el método de trisección para encontrar una raíz de la función f en el intervalo [a, b].
+    
+    Parámetros:
+    f -- La función para la cual se busca la raíz.
+    (a, b) -- Limites de signo opuesto del intervalo.
+    
+    Devuelve:
+    Una float que contiene la raíz encontrada
+    """
+    
+    global globalCountTrisec
 
     if f(a) * f(b) > 0:
         raise ValueError("Para encontrar la raiz, tanto la f(a) como la f(b) deben tener como resultado signos opuestos")
 
     while (abs(b - a)) > e:
-        count = count + 1
+        globalCountTrisec = globalCountTrisec + 1
         c = a + (b - a) / 3.0
         d = a + 2 * (b - a) / 3.0
 
@@ -26,29 +44,46 @@ def trisection(f: Callable[[float], float], a: float, b: float) -> float:
         else:
             a = d
 
-    print('El metodo de trisección tuvo ', count, ' iteraciones.')
+    print('El metodo de trisección tuvo ', globalCountTrisec, ' iteraciones.')
     return (a + b) / 2.0
 
-# Se crea variable global como contador de la bisección recursiva
-globalCount = 0
+#******************************************************************************
+#********************************** Bisección *********************************
+#******************************************************************************
 
-def biseccion(f,a, b) -> float:
-    global globalCount
+def bisection(f, a, b) -> float:
+    """
+    Esta función implementa el método de bisección para encontrar una raíz de la función f en el intervalo [a, b].
+    
+    Parámetros:
+    f -- La función para la cual se busca la raíz.
+    (a, b) -- Limites de signo opuesto del intervalo.
+    
+    Devuelve:
+    Una float que contiene la raíz encontrada
+    """
+        
+    global globalCountBisec
 
-    p = (a + b) / 2
+    if f(a) * f(b) > 0:
+        raise ValueError("Para encontrar la raiz, tanto la f(a) como la f(b) deben tener como resultado signos opuestos")
+
+    p = (a + b) / 2.0
 
     if (abs(b - a)) < e:
-        print('El metodo de bisección tuvo ', globalCount, ' iteraciones.')
+        print('El metodo de bisección tuvo ', globalCountBisec, ' iteraciones.')
         return p
     
-    globalCount = globalCount + 1
+    globalCountBisec = globalCountBisec + 1
 
-    if f(p) > 0: 
-        return biseccion(f, a, p)
+    if f(b) * f(p) > 0: 
+        return bisection(f, a, p)
     else:
-        return biseccion(f, p, b)
+        return bisection(f, p, b)
 
-
+#******************************************************************************
+#****************************** Grafico de Lineas *****************************
+#******************************************************************************
 
 def plot_function(f, a, b, func_str):
     # Crea un array de valores x entre a y b
@@ -67,7 +102,26 @@ def plot_function(f, a, b, func_str):
     plt.legend()
     plt.show()
 
+#******************************************************************************
+#****************************** Grafico de Torta ******************************
+#******************************************************************************
 
+def plot_pie_chart():
+    labelTrisec = 'Trisección (' + str(globalCountTrisec) + ')'
+    labelBisec = 'Bisección (' + str(globalCountBisec) + ')'
+    labels = [labelTrisec, labelBisec]
+    sizes = [globalCountTrisec, globalCountBisec]
+    explode = [0.05, 0.05]
+
+    #Implementa un grafico de torta con las configuraciones seteadas
+    plt.pie(sizes, labels=labels, explode=explode, autopct="%1.1f%%", shadow=True, startangle=90, colors=['pink', 'yellow'])
+    plt.title('Numero de Iteraciones')
+    plt.legend()
+    plt.show() #Muestra el grafico anteriormente seteado
+
+#******************************************************************************
+#***************************** Recolección de Datos ***************************
+#******************************************************************************
 
 func_str = input("Ingresa la función f en términos de x: ")
 f = lambda x: eval(func_str)
@@ -76,11 +130,17 @@ a = float(input("Ingresa el valor de a: "))
 b = float(input("Ingresa el valor de b: "))
 e = float(input("Ingresa la tolerancia: "))
 
+
+#******************************************************************************
+#***************************** Llamada a funciones ****************************
+#******************************************************************************
+
 print("Una posible raíz para el metodo de trisección es: ", trisection(f, a, b))
-print("Una posible raíz para el metodo de bisección es: ", biseccion(f, a , b))
-print('Cierre el grafico para finalizar el programa.')
+print("Una posible raíz para el metodo de bisección es: ", bisection(f, a , b))
+print('Cierre los graficos para finalizar el programa.')
 
 plot_function(f, a, b, func_str)
+plot_pie_chart()
 
 #******************************************************************************
 #******************************** Observaciones *******************************
@@ -88,6 +148,4 @@ plot_function(f, a, b, func_str)
 
 # Se implementa el metodo de triseccion y el de biseccion para comparar la eficiacia de ambos metodos entre si
 # Se puede notar que generalmente el metodo de trisección necesita aproximadamente un 1.62 menos de iteraciones 
-# que la biseccion (con recursividad) para encontrar la raiz de la función. Esto no implica que sea mas eficaz
-# ya que tiene una cantidad mayor de condiciones y calculos.
-# Si bien depende mucho del problema a resolver, la biseccion tiende a ser mas eficaz que triseccion
+# que la biseccion para encontrar la raiz de la función. Esto no implica que sea mas performante ya que cada iteración es mas costosa
